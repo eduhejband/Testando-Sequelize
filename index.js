954954ded13d885b88e806afae57915df012d352
Eduardo <eduardohejband@gmail.com>
2023-03-21 19:29:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const sequelize = new Sequelize('postgres', 'postgres', 'Guarei2022', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
+const Usuario = sequelize.define('usuarios', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    usuario: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    senha: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    timestamps: false // desabilita as colunas createdAt e updatedAt
+  });
+
+  async function cadastrarUsuarios() {
+    try {
+      await sequelize.authenticate();
+      console.log('Connection has been established successfully.');
+  
+      const usuarios = await Usuario.create({
+          id:3,
+          usuario: 'John Doe',
+          senha: '1234567'
+        }).then(usuario => {
+          console.log(usuario.toJSON());
+        });
+    } catch (error) {
+      console.error('Unable to connect to the database:', error);
+    }
+  }
+
+async function consultaUsuarios() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+
+    const usuarios = await Usuario.findAll();
+    console.log(usuarios);
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+consultaUsuarios();
+
